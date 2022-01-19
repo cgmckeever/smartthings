356ccbde7ed41ebb4e6a8a838beda980a9ea6ebc
Chris McKeever <cgmckeever@rebelliondefense.com>
2022-01-19 10:57:57 -0500

    hubitat backup

--- hubitat-backup.sh
+++ hubitat-backup.sh
@@ -6,10 +6,11 @@ he_login="username"
 he_passwd="password"
 he_ipaddr="IP.address"
 cookiefile="/tmp/hubitat.cookie"
-backupdir="/home/pi/backup"
+backupdir="/path/to/backup"
+backupfile=Hubitat_$(date +%Y-%m-%d-%H%M).lzf
 
 find $backupdir/*.lzf -mtime +5 -exec rm {} \;
 curl -k -c $cookiefile -d username=$he_login -d password=$he_passwd https://$he_ipaddr/login
-curl -k -sb $cookiefile https://$he_ipaddr/hub/backup | awk -F'["=]' '/class=.download/ { file=$9 } END { print file }' | xargs -I@ curl -k -sb $cookiefile https://$he_ipaddr/hub//backupDB?fileName=@ -o $backupdir/Hubitat_$(date +%Y-%m-%d-%H%M).lzf
+curl -k -sb $cookiefile https://$he_ipaddr/hub/backup | awk -F'["=]' '/class=.download/ { file=$9 } END { print file }' | xargs -I@ curl -k -sb $cookiefile https://$he_ipaddr/hub//backupDB?fileName=@ -o $backupdir/$backupfile
 
 rm $cookiefile
